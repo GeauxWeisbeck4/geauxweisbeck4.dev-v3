916bb2bf207dba57a7463d3ef936344ee44f6120
GeauxWeisbeck4 <andrew.weisbeck@gmail.com>
2024-11-13 22:17:30 -0500

    Added contact page and form
    A: Added contact page and form

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,10 +5,12 @@ import tailwind from '@astrojs/tailwind';
 
 import react from '@astrojs/react';
 
+import icon from 'astro-icon';
+
 // https://astro.build/config
 export default defineConfig({
   prefetch: {
     prefetchAll: true
   },
-  integrations: [tailwind(), react()]
+  integrations: [tailwind(), react(), icon()]
 });
